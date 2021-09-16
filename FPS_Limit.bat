ede47f3c0d1632fb7947cbbaddd2f7c926a95730
Laurens Voerman <l.voerman@rug.nl>
2021-09-16 17:36:19 +0200

    find ue4 install location in registry

--- FPS_Limit.bat
+++ FPS_Limit.bat
@@ -1,6 +1,17 @@
-cd /D "%~dp0"
+@cd /D "%~dp0"
+@SET "UE_4_26=C:\Program Files\Epic Games\UE_4.26\Engine\Binaries\Win64\UE4Editor.exe"
+
+@FOR /F "usebackq tokens=1,2* " %%i in (`reg QUERY "HKLM\SOFTWARE\EpicGames\Unreal Engine\4.26" /v "InstalledDirectory" 2^> NUL`) do @(
+@if %%i==InstalledDirectory @if %%j==REG_SZ set "UE_4_26=%%k\Engine\Binaries\Win64\UE4Editor.exe"
+)
+@if not exist "%UE_4_26%" @(
+@echo UE4Editor not found; "%UE_4_26%"
+@GOTO :EOF
+)
+@echo UE4Editor found:  "%UE_4_26%"
+
 for %%i in (*.uproject) do (
-start "UE4" /b "C:\Program Files\Epic Games\UE_4.26\Engine\Binaries\Win64\UE4Editor.exe" "%~dp0%%i" -ExecCmds="t.MaxFPS 30"
+start "UE4" /b "%UE_4_26%" "%~dp0%%i" -ExecCmds="t.MaxFPS 30"
 rem start "UE4" /b "argprint.exe" "%~dp0%%i" -ExecCmds="t.MaxFPS 30"
-goto EOF
+GOTO :EOF
 )
