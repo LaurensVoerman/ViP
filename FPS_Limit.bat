da3d924d3be76eac77fd500b86282c278c432eb5
Laurens Voerman <l.voerman@rug.nl>
2022-01-11 16:51:34 +0100

    first attempt to add fluid to dropfunnel.

--- FPS_Limit.bat
+++ FPS_Limit.bat
@@ -1,14 +1,14 @@
 @cd /D "%~dp0"
-@SET "UE_4_26=C:\Program Files\Epic Games\UE_4.26\Engine\Binaries\Win64\UE4Editor.exe"
+@SET "UE_4_27=C:\Program Files\Epic Games\UE_4.27\Engine\Binaries\Win64\UE4Editor.exe"
 
-@FOR /F "usebackq tokens=1,2* " %%i in (`reg QUERY "HKLM\SOFTWARE\EpicGames\Unreal Engine\4.26" /v "InstalledDirectory" 2^> NUL`) do @(
-@if %%i==InstalledDirectory @if %%j==REG_SZ set "UE_4_26=%%k\Engine\Binaries\Win64\UE4Editor.exe"
+@FOR /F "usebackq tokens=1,2* " %%i in (`reg QUERY "HKLM\SOFTWARE\EpicGames\Unreal Engine\4.27" /v "InstalledDirectory" 2^> NUL`) do @(
+@if %%i==InstalledDirectory @if %%j==REG_SZ set "UE_4_27=%%k\Engine\Binaries\Win64\UE4Editor.exe"
 )
-@if not exist "%UE_4_26%" @(
-@echo UE4Editor not found; "%UE_4_26%"
+@if not exist "%UE_4_27%" @(
+@echo UE4Editor not found; "%UE_4_27%"
 @GOTO :EOF
 )
-@echo UE4Editor found:  "%UE_4_26%"
+@echo UE4Editor found:  "%UE_4_27%"
 rem fake builddata
 for %%i in (Content\VirtualRealityBP\Maps\*.umap) do (
   	if not exist Content\VirtualRealityBP\Maps\%%~ni_BuiltData.uasset (
@@ -17,7 +17,7 @@ for %%i in (Content\VirtualRealityBP\Maps\*.umap) do (
 )
 
 for %%i in (*.uproject) do (
-start "UE4" /b "%UE_4_26%" "%~dp0%%i" -ExecCmds="t.MaxFPS 30"
+start "UE4" /b "%UE_4_27%" "%~dp0%%i" -ExecCmds="t.MaxFPS 30"
 rem start "UE4" /b "argprint.exe" "%~dp0%%i" -ExecCmds="t.MaxFPS 30"
 GOTO :EOF
 )
