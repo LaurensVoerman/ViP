fb537fe2c62b3bbf9acaa393588b19e5d95345cc
Laurens Voerman <l.voerman@rug.nl>
2022-07-01 11:46:25 +0200

    3d tag  update (912)

--- FPS_Limit.bat
+++ FPS_Limit.bat
@@ -1,14 +1,13 @@
 @cd /D "%~dp0"
-@SET "UE_4_27=C:\Program Files\Epic Games\UE_4.27\Engine\Binaries\Win64\UE4Editor.exe"
-
+@SET "UE_4=C:\Program Files\Epic Games\UE_4.27\Engine\Binaries\Win64\UE4Editor.exe"
 @FOR /F "usebackq tokens=1,2* " %%i in (`reg QUERY "HKLM\SOFTWARE\EpicGames\Unreal Engine\4.27" /v "InstalledDirectory" 2^> NUL`) do @(
-@if %%i==InstalledDirectory @if %%j==REG_SZ set "UE_4_27=%%k\Engine\Binaries\Win64\UE4Editor.exe"
+@if %%i==InstalledDirectory @if %%j==REG_SZ set "UE_4=%%k\Engine\Binaries\Win64\UE4Editor.exe"
 )
-@if not exist "%UE_4_27%" @(
-@echo UE4Editor not found; "%UE_4_27%"
+@if not exist "%UE_4%" @(
+@echo UE4Editor not found; "%UE_4%"
 @GOTO :EOF
 )
-@echo UE4Editor found:  "%UE_4_27%"
+@echo UE4Editor found:  "%UE_4%"
 rem fake builddata
 for %%i in (Content\VirtualRealityBP\Maps\*.umap) do (
   	if not exist Content\VirtualRealityBP\Maps\%%~ni_BuiltData.uasset (
@@ -17,7 +16,7 @@ for %%i in (Content\VirtualRealityBP\Maps\*.umap) do (
 )
 
 for %%i in (*.uproject) do (
-start "UE4" /b "%UE_4_27%" "%~dp0%%i" -ExecCmds="t.MaxFPS 30"
+start "UE4" /b "%UE_4%" "%~dp0%%i" -ExecCmds="t.MaxFPS 30"
 rem start "UE4" /b "argprint.exe" "%~dp0%%i" -ExecCmds="t.MaxFPS 30"
 GOTO :EOF
 )
